6fe4f7d0837e39c812393bd779954ab26d9cdbc4
Adnan Akbar <adnan.akbar@itaas.dimensiondata.com>
2017-01-05 14:05:12 +1100

    operating system list options filters added.

--- ComputeClient/Compute.Contracts/Requests/Infrastructure/OperatingSystemListOptions.cs
+++ ComputeClient/Compute.Contracts/Requests/Infrastructure/OperatingSystemListOptions.cs
@@ -20,10 +20,20 @@
         /// </summary>
         public const string FamilyField = "family";
 
-        /// <summary>
-        /// Gets or sets the id filter.
-        /// </summary>
-        public string Id
+		/// <summary>
+		/// The "DatacenterId" field name.
+		/// </summary>
+		public const string DatacenterIdField = "datacenterId";
+
+		/// <summary>
+		/// The "SupportsGuestOsCustomization" field name.
+		/// </summary>
+		public const string SupportsGuestOsCustomizationField = "supportsGuestOsCustomization";
+
+		/// <summary>
+		/// Gets or sets the id filter.
+		/// </summary>
+		public string Id
         {
             get { return GetFilter<string>(IdField); }
             set { SetFilter(IdField, value); }
@@ -46,5 +56,23 @@
             get { return GetFilter<string>(FamilyField); }
             set { SetFilter(FamilyField, value); }
         }
-    }
+
+		/// <summary>
+		/// Gets or sets the Datacenter Id filter.
+		/// </summary>
+		public string DatacenterId
+		{
+			get { return GetFilter<string>(DatacenterIdField); }
+			set { SetFilter(DatacenterIdField, value); }
+		}
+
+		/// <summary>
+		/// Gets or sets the Supports Guest Os Customization filter.
+		/// </summary>
+		public string SupportsGuestOsCustomization
+		{
+			get { return GetFilter<string>(SupportsGuestOsCustomizationField); }
+			set { SetFilter(SupportsGuestOsCustomizationField, value); }
+		}
+	}
 }
