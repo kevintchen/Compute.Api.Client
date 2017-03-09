2c76c413a8a10c27deedd19d2f1fa75a17d3d6c2
Hamed Soleymani <hamed.soleymani@itaas.dimensiondata.com>
2017-03-09 12:28:58 +1100

    Update add disk contract

--- ComputeClient/Compute.Client/ApiUris.cs
+++ ComputeClient/Compute.Client/ApiUris.cs
@@ -2190,7 +2190,7 @@ namespace DD.CBU.Compute.Api.Client
         /// <returns>The <see cref="Uri"/>.</returns>
         public static Uri AddDisk(Guid orgId)
         {
-            return new Uri(string.Format(MCP2_4_PREFIX + "{0}/server/addDisk", orgId), UriKind.Relative);
+            return new Uri(string.Format(MCP2_5_PREFIX + "{0}/server/addDisk", orgId), UriKind.Relative);
         }
 
         /// <summary>Returns the relative URI of the REST request for removing disk from server.</summary>
