035a8a94ce44469b3f1e47bb61a6e1983b940a7a
Hamed Soleymani <hamed.soleymani@itaas.dimensiondata.com>
2017-05-02 10:23:27 +1000

    Handel Sata/Ide disk and driver by casting the items by type

---
+++ ComputeClient/Compute.Contracts/Image20/HistoricalImageExportsOverride.cs
@@ -0,0 +1,56 @@
+﻿using DD.CBU.Compute.Api.Contracts.Network20;
+
+namespace DD.CBU.Compute.Api.Contracts.Image20
+{
+    public partial class HistoricalImageExportIdeControllerType
+    {
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        public HistoricalImageExportIdeDeviceType[] device
+        {
+            get { return Items as HistoricalImageExportIdeDeviceType[]; }
+            set
+            {
+                if (value != null || Items is HistoricalImageExportIdeDeviceType[])
+                    Items = value as HistoricalImageExportIdeDeviceType[];
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        public HistoricalImageExportIdeDiskType[] disk
+        {
+            get { return Items as HistoricalImageExportIdeDiskType[]; }
+            set
+            {
+                if (value != null || Items is HistoricalImageExportIdeDiskType[])
+                    Items = value as HistoricalImageExportIdeDiskType[];
+            }
+        }
+    }
+
+    public partial class HistoricalImageExportSataControllerType
+    {
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        public HistoricalImageExportSataDeviceType[] device
+        {
+            get { return Items as HistoricalImageExportSataDeviceType[]; }
+            set
+            {
+                if (value != null || Items is HistoricalImageExportSataDeviceType[])
+                    Items = value as HistoricalImageExportSataDeviceType[];
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        public HistoricalImageExportSataDiskType[] disk
+        {
+            get { return Items as HistoricalImageExportSataDiskType[]; }
+            set
+            {
+                if (value != null || Items is HistoricalImageExportSataDiskType[])
+                    Items = value as HistoricalImageExportSataDiskType[];
+            }
+        }
+    }
+}
