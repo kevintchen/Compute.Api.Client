1007f6dc7879103ca8ea3e77254c230841e97aad
Hamed Soleymani <hamed.soleymani@itaas.dimensiondata.com>
2017-05-03 10:57:41 +1000

    Update the device and disk in Storage controller casting type

--- ComputeClient/Compute.Contracts/Image20/HistoricalImageExportsOverride.cs
+++ ComputeClient/Compute.Contracts/Image20/HistoricalImageExportsOverride.cs
@@ -6,80 +6,20 @@ namespace DD.CBU.Compute.Api.Contracts.Image20
     public partial class HistoricalImageExportIdeControllerType
     {
         [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public HistoricalImageExportIdeDeviceType[] device
-        {
-            get
-            {
-                return Items != null && Items.Any(x => x.GetType() == typeof(HistoricalImageExportIdeDeviceType))
-                    ? (Items.Where(x => x.GetType() == typeof(HistoricalImageExportIdeDeviceType)).ToArray()) as
-                        HistoricalImageExportIdeDeviceType[]
-                    : null;
-
-            }
-            set
-            {
-                if (value != null || Items is HistoricalImageExportIdeDeviceType[])
-                    Items = value as HistoricalImageExportIdeDeviceType[];
-            }
-        }
+        public ImageIdeDeviceType[] device => Items?.OfType<ImageIdeDeviceType>().ToArray();
 
         /// <remarks/>
         [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public HistoricalImageExportIdeDiskType[] disk
-        {
-            get
-            {
-                return Items != null && Items.Any(x => x.GetType() == typeof(HistoricalImageExportIdeDiskType))
-                    ? (Items.Where(x => x.GetType() == typeof(HistoricalImageExportIdeDiskType)).ToArray()) as
-                        HistoricalImageExportIdeDiskType[]
-                    : null;
-
-            }
-            set
-            {
-                if (value != null || Items is HistoricalImageExportIdeDiskType[])
-                    Items = value as HistoricalImageExportIdeDiskType[];
-            }
-        }
+        public ImageIdeDiskType[] disk => Items?.OfType<ImageIdeDiskType>().ToArray();
     }
 
     public partial class HistoricalImageExportSataControllerType
     {
         [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public HistoricalImageExportSataDeviceType[] device
-        {
-            get
-            {
-                return Items != null && Items.Any(x => x.GetType() == typeof(HistoricalImageExportSataDeviceType))
-                    ? (Items.Where(x => x.GetType() == typeof(HistoricalImageExportSataDeviceType)).ToArray()) as
-                        HistoricalImageExportSataDeviceType[]
-                    : null;
-
-            }
-            set
-            {
-                if (value != null || Items is HistoricalImageExportSataDeviceType[])
-                    Items = value as HistoricalImageExportSataDeviceType[];
-            }
-        }
+        public HistoricalImageExportSataDeviceType[] device => Items?.OfType<HistoricalImageExportSataDeviceType>().ToArray();
 
         /// <remarks/>
         [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public HistoricalImageExportSataDiskType[] disk
-        {
-            get
-            {
-                return Items != null && Items.Any(x => x.GetType() == typeof(HistoricalImageExportSataDiskType))
-                    ? (Items.Where(x => x.GetType() == typeof(HistoricalImageExportSataDiskType)).ToArray()) as
-                        HistoricalImageExportSataDiskType[]
-                    : null;
-
-            }
-            set
-            {
-                if (value != null || Items is HistoricalImageExportSataDiskType[])
-                    Items = value as HistoricalImageExportSataDiskType[];
-            }
-        }
+        public HistoricalImageExportSataDiskType[] disk => Items?.OfType<HistoricalImageExportSataDiskType>().ToArray();
     }
 }
--- ComputeClient/Compute.Contracts/Network20/ImageControllerOverride.cs
+++ ComputeClient/Compute.Contracts/Network20/ImageControllerOverride.cs
@@ -6,80 +6,20 @@ namespace DD.CBU.Compute.Api.Contracts.Network20
     public partial class ImageSataControllerType
     {
         [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public ImageSataDeviceType[] device
-        {
-            get
-            {
-                return Items != null && Items.Any(x => x.GetType() == typeof(ImageSataDeviceType))
-                    ? (Items.Where(x => x.GetType() == typeof(ImageSataDeviceType)).ToArray()) as
-                        ImageSataDeviceType[]
-                    : null;
-
-            }
-            set
-            {
-                if (value != null || Items is ImageSataDeviceType[])
-                    Items = value as ImageSataDeviceType[];
-            }
-        }
+        public ImageSataDeviceType[] device => Items?.OfType<ImageSataDeviceType>().ToArray();
 
         /// <remarks/>
         [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public ImageSataDiskType[] disk
-        {
-            get
-            {
-                return Items != null && Items.Any(x => x.GetType() == typeof(ImageSataDiskType))
-                    ? (Items.Where(x => x.GetType() == typeof(ImageSataDiskType)).ToArray()) as
-                        ImageSataDiskType[]
-                    : null;
-
-            }
-            set
-            {
-                if (value != null || Items is ImageSataDiskType[])
-                    Items = value as ImageSataDiskType[];
-            }
-        }
+        public ImageSataDiskType[] disk => Items?.OfType<ImageSataDiskType>().ToArray();
     }
 
     public partial class ImageIdeControllerType
     {
         [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public ImageIdeDeviceType[] device
-        {
-            get
-            {
-                return Items != null && Items.Any(x => x.GetType() == typeof(ImageIdeDeviceType))
-                    ? (Items.Where(x => x.GetType() == typeof(ImageIdeDeviceType)).ToArray()) as
-                        ImageIdeDeviceType[]
-                    : null;
-
-            }
-            set
-            {
-                if (value != null || Items is ImageIdeDeviceType[])
-                    Items = value as ImageIdeDeviceType[];
-            }
-        }
+        public ImageIdeDeviceType[] device => Items?.OfType<ImageIdeDeviceType>().ToArray();
 
         /// <remarks/>
         [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public ImageIdeDiskType[] disk
-        {
-            get
-            {
-                return Items != null && Items.Any(x => x.GetType() == typeof(ImageIdeDiskType))
-                    ? (Items.Where(x => x.GetType() == typeof(ImageIdeDiskType)).ToArray()) as
-                        ImageIdeDiskType[]
-                    : null;
-
-            }
-            set
-            {
-                if (value != null || Items is ImageIdeDiskType[])
-                    Items = value as ImageIdeDiskType[];
-            }
-        }
+        public ImageIdeDiskType[] disk => Items?.OfType<ImageIdeDiskType>().ToArray();
     }
 }
